1e94bbbf2b3a20f77200890c34d46b238c28217b
pastelmind <keepyourhonor@gmail.com>
2020-01-29 05:12:27 +0900

    Bump to v0.2.0

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("README.md", "r") as readme_md:
 
 setuptools.setup(
     name="d2txt",
-    version="0.1.0",
+    version="0.2.0",
     author="Yehyoung Kang",
     author_email="keepyourhonor@gmail.com",
     description="Parses Diablo II's TXT files and converts them to INI files",
